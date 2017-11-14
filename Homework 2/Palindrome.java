255b298f0c29820d5715f9ed996447ee6879c9e9
Artem Predvoditelev <vladglad39@gmail.com>
2017-11-15 00:55:01 +0200

    Refactoring a class "Palindrome"

--- Homework 2/Palindrome.java
+++ Homework 2/Palindrome.java
@@ -3,10 +3,9 @@ import java.util.Scanner;
 public class Palindrome {
 
     public static void main(String[] args) {
-        System.out.printf("Введите строку, чтобы проверить является ли она палиндромом: ");
+        System.out.printf("Введите строку, что бы проверить является ли она палиндромом: ");
         Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
-        isPalindrome(str);
+        isPalindrome(scanner.nextLine());
     }
 
     private static void isPalindrome(String str) {
